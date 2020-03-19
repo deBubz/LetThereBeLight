9fc060a8ac39e1b00178c7a80abdec21186c430b
debubz <bao.es.hoang2@gmail.com>
2020-03-19 20:10:52 +1100

    fix styling

--- studio_notes/Week3/dbQuickStart/dbFunctions.js
+++ studio_notes/Week3/dbQuickStart/dbFunctions.js
@@ -1,77 +1,75 @@
-const assert = require('assert');
-// const collection = db.collection('sensorreadings');
-
+const assert = require("assert");
 
 exports.module = {
-    insertDoc: function (db, callback){
+    insertDoc: (db, callback) => {
         const collection = db.collection('sensorreadings');
         // inserting
         collection.insertMany([
             {a:1},{a:2},{a:3} // insert these 3 args
-        ], (err, result)=>{
+        ], (err, result) => {
             assert.equal(err, null);
             assert.equal(3, result.result.n);
             assert.equal(3, result.ops.length);     // ops - contain the docs inserted with the added __id fields
-            console.log("Inserted 3 documents into the collection");
+            Console.log("Inserted 3 documents into the collection");
             callback(result);
         })
     
     },
     
-    findDocs : function (db, callback){    // find all no pring
+    findDocs : (db, callback) => {    // find all no pring
         const collection = db.collection('sensorreadings');
     
         // find returns cursor object
-        collection.find({}).each((err,docs)=>{ //can also do  find().toarray()
+        collection.find({}).each((err,docs) => { //can also do  find().toarray()
             assert.equal(err, null);
-            console.log("found these records");
-            console.log(docs);
+            Console.log("found these records");
+            Console.log(docs);
             callback(docs);
             // only find , didnot print
         })
     },
     
-    findDocQuery : function(db, callback){    // find matching query
+    findDocQuery :(db, callback) => {    // find matching query
         const collection = db.collection('sensorreadings');
     
-        collection.find({a:1}).toArray((err,docs)=>{
+        collection.find({a:1}).toArray((err,docs) => {
             assert.equal(err,null);
-            console.log("found these");
-            console.log(docs);
+            Console.log("found these");
+            Console.log(docs);
             callback(docs);
         });
     },
     
-    updateOneDoc : function (db, callback){ // update one with a:1
+    updateOneDoc : (db, callback) => { // update one with a:1
         const collection = db.collection('sensorreadings');
     
         collection.updateOne( {a:1}, {$set:{b:1}}, (err, result)=>{
             assert.equal(err, null);
             assert.equal(1, result.result.n);
-            console.log("updated");
+            Console.log("updated");
             callback(result);
-            console.toArraylog(result);
+            Console.toArraylog(result);
             
         });
         
     },
     
-    removeOneDoc: function (db, callback){ // deleted one
+    removeOneDoc: (db, callback) => { // deleted one
         const collection = db.collection('sensorreadings');
     
-        collection.deleteOne({a:3}, (err,result)=>{
+        collection.deleteOne({a:3}, (err,result) =>{ 
             assert.equal(err, null);
             assert.equal(1, result.result.n);
-            console.log('removed something');
+            Console.log('removed something');
             callback(result);
             
-        })
+        });
     },
     
-    indexCollection:function (db,callback){  // something index ,increases your performance
+    indexCollection: (db,callback) => {  // something index ,increases your performance
         db.collection('sensorreadings').createIndex(
-            {"a":1}, null, (err, result)=>{
-                console.log(result);
+            {"a":1}, null, (err, result) => {
+                Console.log(result);
                 callback(result);
                 
             }
